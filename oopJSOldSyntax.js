b85dc00e40fdc78f7139d0aade2e016241412013
Zach <of7.ztaylor@gmail.com>
2019-06-27 01:46:05 -0500

    Clean Up
    Added comments && cleaned up code. Also included his original solution to removeManagedMembers.

--- oopJSOldSyntax.js
+++ oopJSOldSyntax.js
@@ -1,8 +1,20 @@
+/*
+Credit: @WellPaidGeek
+Purpose: Read his articles on OOP in JS to help me better wrap my head
+          around OOP in JS that I've been working through in FCC.
+          Recreated his code example from memory.
+          Not gonna lie, I had a small hiccup in the return of 
+          generatePayslip.
+          Overall, I'm pretty proud of how I did. Now I'm off to 
+          refactor this in ES6 syntax.
+*/
+
+//Base class for employees.
 function Employee(id, firstName, lastName, socialSecurityNum, salary) {
-    this.id = id,
-    this.firstName = firstName,
-    this.lastName = lastName,
-    this.socialSecurityNum = socialSecurityNum,
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.socialSecurityNum = socialSecurityNum;
     this.salary = salary;
 }
 
@@ -14,28 +26,40 @@ Employee.prototype.generatePayslip = function() {
         + '\n'
         + 'Social Security Number: ' + this.socialSecurityNum
         + '\n'
-        + 'Salary: ' + pay;
+        + 'Pay: $' + pay;
 }
 
+//Manager class, extends employee.
 function Manager (id, firstName, lastName, socialSecurityNum, salary) {
     Employee.call(this, id, firstName, lastName, socialSecurityNum, salary);
     this.managedEmployees = [];
 }
 
+//Inherits methods from Employee.
 Manager.prototype = Object.create(Employee.prototype);
 
+//Adds own methods for new properties.
 Manager.prototype.addManagedEmployee = function (employee) {
     this.managedEmployees.push(employee);
 }
 
+//My solution to this was a little different from his.
+//His was
+//this.managedEmployees.filter(function (e) { return e.id !== employee.id });
 Manager.prototype.removeManagedEmployee = function (employee) {
     return this.managedEmployees.filter(e => e.id !== employee.id);
 }
 
+//Quick detour to fix the constructor.
 Manager.constructor = Manager;
 
+//Actual logic displaying results in the console.
 var oneEmployee = new Employee(23, 'Carlos', 'Rual', 0001, 60000);
 var theManager = new Manager(3, 'Jerome', 'Williams', 1020, 80000);
 
 console.log(oneEmployee.generatePayslip());
 console.log(theManager.generatePayslip());
+theManager.addManagedEmployee(oneEmployee);
+console.log(theManager.managedEmployees);
+theManager.removeManagedEmployee(oneEmployee);
+console.log(theManager.managedEmployees);
