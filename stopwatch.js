93b5c1a048336a6199621ef42c4e1fbfb31091ee
Zach <of7.ztaylor@gmail.com>
2019-07-20 17:42:32 -0500

    Minor work
    SW made, called in Index for work. SW also reworked for specific challenge, needs refactoring again to a more ideal setup.

--- stopwatch.js
+++ stopwatch.js
@@ -4,39 +4,43 @@ Develop a StopWatch using OOP in JS.
 
 function StopWatch() {
     //variables
-    let startTime, endTime, status, duration = 0;
-//}
-
-    //Methods
-    this.start = function(){
-        if (status) {
-            throw new Error('Start already pressed!');
-        } 
-            status = true;
-            startTime = new Date();
-    }
-
-    this.stop = function(){
-        if (!status) {
-            throw new Error('Stop already pressed!');
-        }  
-        status = false;
-        endTime = new Date();
-        const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
-        duration += seconds;
-    }
-
-    this.reset = function() {
-        status = false;
-        startTime = null;
-        endTime = null;
-        duration = 0;
-    }
+    //let startTime, endTime, status, duration = 0;
+    this.startTime = 0;
+    this.endTime = 0;
+    this.status = 0;
+    this.duration = 0;
 
     // Getter
-    Object.defineProperty(this, 'duration', {
-        get: function() { return duration; }
-    }); 
+    // Object.defineProperty(this, 'duration', {
+    //     get: function() { return duration; }
+    // }); 
 }
 
-const sw = new StopWatch();
+    //Methods
+    StopWatch.prototype.start = function(){
+        if (this.status) {
+            throw new Error('Start already pressed!');
+        } 
+            this.status = true;
+            this.startTime = new Date();
+    }
+
+    StopWatch.prototype.stop = function(){
+        if (!this.status) {
+            throw new Error('Stop already pressed!');
+        }  
+        this.status = false;
+        this.endTime = new Date();
+        const seconds = (this.endTime.getTime() - this.startTime.getTime()) / 1000;
+        this.duration += seconds;
+    }
+
+    StopWatch.prototype.reset = function() {
+        this.status = false;
+        this.startTime = null;
+        this.endTime = null;
+        this.duration = 0;
+    }
+//}
+
+let sw = new StopWatch();
