e72ee4dc9a03c5a06169e92e78c3618c37f567b1
Zach <of7.ztaylor@gmail.com>
2019-07-23 19:03:56 -0500

    Polymorphism Base
    First run, needs refactoring.

--- prototypicalinhertiance.js
+++ prototypicalinhertiance.js
@@ -1,11 +1,12 @@
-// HTMLElement && HTMLSelectElement
-
+// Base Element
 function HTMLElementOwn() {
     this.click = function() { console.log('click!'); }
 }
 
 HTMLElementOwn.prototype.focus = function() { console.log('Focused'); }
+HTMLElementOwn.prototype.render = function() { return 'render'; }
 
+// First child element.
 function HTMLSelectElementOwn(items = []) {
     this.items = [];
     this.addItem = function (item){ this.items.push(item); };
@@ -14,3 +15,32 @@ function HTMLSelectElementOwn(items = []) {
 
 HTMLSelectElementOwn.prototype = new HTMLElementOwn();
 HTMLSelectElementOwn.prototype.constructor = HTMLSelectElementOwn;
+
+HTMLSelectElementOwn.prototype.render = function() {
+    return '<select>\n';
+    // + for (item in this.items) {
+    //     <option>
+    // }
+    // + '';
+}
+
+function HTMLImg(address = 'https://www.duckduckgo.com') {
+    this.address = address;
+}
+
+HTMLImg.prototype = new HTMLElementOwn();
+HTMLImg.prototype.constructor = HTMLImg;
+
+HTMLImg.prototype.render = function() {
+    return 'src=\"' + this.address + '\"';
+}
+
+const s = new HTMLImg();
+
+const elements = [
+    new HTMLSelectElementOwn([1, 2, 3]),
+    new HTMLImg('http://')
+];
+
+for (let element of elements)
+    console.log(element.render());
