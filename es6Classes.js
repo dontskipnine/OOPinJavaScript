15af409edcdb9a49e35bfa97e61b73d444808c5e
Zach <of7.ztaylor@gmail.com>
2019-07-25 17:02:30 -0500

    Groundwork
    Ground work laid, need to flesh out the pop/push/peek w/ WeakMap. Also, updated index.

--- es6Classes.js
+++ es6Classes.js
@@ -3,6 +3,28 @@ const _stack = new WeakMap();
 
 class Stack2 {
     constructor() {
-        //
+        _count.set(this, 0);
+        _stack.set(this, []);
     }
+
+    peek() {
+        let num = _count.get(this) - 1;
+        return _stack.get(this)[num];
+    }
+
+    pop() {
+        if (count <= 0) { throw new Error('Stack is empty!'); }
+        let num = _count.get(this) - 1;
+        return _stack.get(this).pop[num];
+    }
+
+    push(value) {
+        let num = _count.get(this) - 1;
+        _stack.set(this).push(value);
+        _count.set(this, + 1);
+    }
+    
+    get count() { return _count.get(this); }
 }
+
+const stack3 = new Stack2();
